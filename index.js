d0164cda29fc4f48d92903e8bd02534034ddd76e
panwilku <marcin.wilk.503@gmail.com>
2025-07-28 13:15:15 +0900

    app rewritten in express

--- index.js
+++ index.js
@@ -1,72 +1,31 @@
-import { createServer } from 'node:http';
-import * as fs from 'node:fs/promises';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
-const hostname = '127.0.0.1';
+const express = require('express');
+const app = express();
 const port = 3000;
 
-const server = createServer((req, res) => {
-    if (req.method === 'GET' && req.url === '/') {
-        res.statusCode = 200;
-        const filePath = path.join(__dirname, 'index.html');
 
-        fs.readFile(filePath, 'utf8')
-        .then(data => {
-            res.setHeader('Content-Type', 'text/html');
-            res.end(data);
-        }).catch(err => {
-            res.statusCode = 500;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('There is an error');
-        })
-    } else if (req.method === 'GET' && req.url === '/contact-me') {
-        res.statusCode = 200;
-        const filePath = path.join(__dirname, 'contact-me.html');
+app.get('/', (req, res) => {
+  res.sendFile("./index.html", { root: __dirname });
+});
 
-        fs.readFile(filePath, 'utf8')
-        .then(data => {
-            res.setHeader('Content-Type', 'text/html');
-            res.end(data);
-        }).catch(err => {
-            res.statusCode = 500;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('There is an error');
-        })
-    } else if (req.method === 'GET' && req.url === '/about') {
-        res.statusCode = 200;
-        const filePath = path.join(__dirname, 'about.html');
+app.get('/about', (req, res) => {
+  res.sendFile("./about.html", { root: __dirname });
+});
 
-        fs.readFile(filePath, 'utf8')
-        .then(data => {
-            res.setHeader('Content-Type', 'text/html');
-            res.end(data);
-        }).catch(err => {
-            res.statusCode = 500;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('There is an error');
-        })
-    } else {
-            res.statusCode = 404;
-            const filePath = path.join(__dirname, '404.html');
-    
-            fs.readFile(filePath, 'utf8')
-            .then(data => {
-                res.setHeader('Content-Type', 'text/html');
-                res.end(data);
-            }).catch(err => {
-                res.statusCode = 500;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('There is an error');
-            })
+app.get('/contact-me', (req, res) => {
+  res.sendFile("./contact-me.html", { root: __dirname });
+});
+
+app.get("*", (req, res) => {
+    try {
+      res.status(404).sendFile("./404.html", { root: __dirname });
+    } catch (err) {
+      console.error("Error sending 404 page:", err);
+      res.status(500).send("Internal Server Error");
     }
+  });
+  
 
-})
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
